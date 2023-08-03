c335dd579e2f0faf2efd4c425d21d9ad1450038c
afshinSh <afshintheprojects@gmail.com>
2023-08-03 09:12:29 +0330

    same with st4ck

--- 0x6.asm
+++ 0x6.asm
@@ -3,13 +3,13 @@ global _start
 _start:
     sub esp, 4      ; => allocate 4 bytes on the stack
     mov [esp], byte 'H'
-    mov [esp], byte 'e'
-    mov [esp], byte 'Y'
-    mov [esp], byte '!'
+    mov [esp+1], byte 'E'
+    mov [esp+2], byte 'Y'
+    mov [esp+3], byte '!'
     mov eax, 4
     mov ebx, 1
     mov ecx, esp    ; => point ecx to the beginning of the string
-    mov edx, 4      ; => length = 4 
+    mov edx, 4     ; => length = 4
     int 0x80        
     mov eax, 1
     mov ebx, 0            
